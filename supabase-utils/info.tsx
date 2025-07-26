abad44b7734e74b0a417d5eda90496fa79412b84
Brittney Leigh Banning <brittneyleighb@gmail.com>
2025-07-26 13:14:17 -0600

    Update info.tsx

--- supabase-utils/info.tsx
+++ supabase-utils/info.tsx
@@ -1,3 +1,5 @@
 // Project configuration
-export const projectId = import.meta.env.VITE_SUPABASE_PROJECT_ID || 'default-project-id';
+export const projectId = 'jvjqfolccxfryxkepnbf';
+
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 export const publicAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
